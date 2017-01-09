900f14bfb45a5b9639e1ddd4bd465895a32bb538
Cphil31 <cecile_philippe@live.fr>
2017-01-09 14:10:33 +0100

    step5 fait

--- index5.php
+++ index5.php
@@ -1,1 +1,12 @@
-<?php  ?>
+<?php 
+$foo=1;
+for ($foo=1; $foo <=15 ; $foo++) { 
+	if($foo<15){
+	echo $foo.'<br/>';	
+	}
+	if($foo===15){
+		echo $foo <15;
+		echo 'On y arrive presque';
+	}
+}
+ ?>
