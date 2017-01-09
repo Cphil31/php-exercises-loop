11f5258ff8045c82b7e0a93377e7d1efab9f9b2d
Cphil31 <cecile_philippe@live.fr>
2017-01-09 11:42:46 +0100

    step 3 fait

---
+++ index3.php
@@ -0,0 +1,26 @@
+<?php 
+
+echo 'step3';
+
+
+
+$foo=10;
+$bar=4;
+$somme=$foo*$bar;
+
+echo 'variable 1='.$foo;
+echo 'variable 2='.$bar;
+
+while($foo<=30){
+	$foo++;
+	echo $foo.'';
+	if ($foo<=10){
+	echo $somme;	
+	}
+	else{
+		
+	}
+
+}
+
+ ?>
