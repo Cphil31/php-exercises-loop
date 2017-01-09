c483ce2127eb3562bf06d5e3b4d4bc97e15cdc71
Cphil31 <cecile_philippe@live.fr>
2017-01-09 11:41:31 +0100

    step 2 fait

---
+++ index2.php
@@ -0,0 +1,24 @@
+<?php 
+
+echo 'step2: ';
+
+$foo=0;
+$bar=5;
+
+$multi=$foo*$bar;
+
+echo '$foo='.$foo;
+echo '$bar='.$bar;
+
+while($foo<=30){
+	$foo++;
+	
+	if ($foo<=20){
+	echo $multi;	
+	}
+	else{
+		echo $foo;
+	}
+}
+
+ ?>
