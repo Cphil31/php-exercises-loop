1da513dfea9dbe465ab1efc79e45f30dbc0dc418
Cphil31 <cecile_philippe@live.fr>
2017-01-09 11:37:06 +0100

    step 3 fait

--- index.php
+++ index.php
@@ -28,4 +28,33 @@ while($foo<=30){
 	}
 }
 
+echo 'step3';
+
+// #Exercice 3
+// Créer deux variables. Initialiser la première à 100 et la deuxième avec un nombre compris en 1 et 100.  
+// Tant que la première variable n'est pas inférieur ou égale à 10 :
+// - multiplier la première variable avec la deuxième
+// - afficher le résultat
+// - décrémenter la première variable
+
+$varTroisUn=10;
+$varTroisDeux=4;
+$sommeTrois=$varTroisUn*$varTroisDeux;
+
+echo 'variable 1='.$varTroisUn;
+echo 'variable 2='.$varTroisDeux;
+
+while($varTroisUn<=30){
+	$varTroisUn++;
+	echo $varTroisUn.'';
+	if ($varTroisUn<=10){
+	echo $sommeTrois;	
+	}
+	else{
+		
+	}
+
+}
+
+
 ?>
