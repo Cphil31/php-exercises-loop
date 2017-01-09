ea92d1b0985eb01e61d5911e2d46b2454bc1861b
Cphil31 <cecile_philippe@live.fr>
2017-01-09 10:53:41 +0100

    step 2 fini

--- index.php
+++ index.php
@@ -7,7 +7,25 @@ while($chiffre<=10){
 	$chiffre++;
 }
 
-echo 'step2';
+echo 'step2: ';
+
 $foo=0;
-$bar=--;
+$bar=5;
+
+$multi=$foo*$bar;
+
+echo '$foo='.$foo;
+echo '$bar='.$bar;
+
+while($foo<=30){
+	$foo++;
+	
+	if ($foo<=20){
+	echo $multi;	
+	}
+	else{
+		echo $foo;
+	}
+}
+
 ?>
